628282d267d753926a9583f41266cf13c6835654
ShafiAhmad251192 <shafiahmad.amu@gmail.com>
2022-07-20 23:46:25 +0530

    Introduction Test

--- project_test/testing_func.py
+++ project_test/testing_func.py
@@ -1,3 +1,9 @@
 # Print name and welcome the person
 def print_name(name):
-    return f"Hello {name}. Welcome to my Daisi account"
+    return f"Hello {name}. Welcome to my Daisi account."
+
+def introduction(name, age):
+    sent1 = print_name(name)
+    sent2 = "You may Introduce yourself to anyone in this way."
+    sent3 = f"My name is {name}. I am {age} years old." 
+    return f"{sent1}\n{sent2}\n{sent3}"
